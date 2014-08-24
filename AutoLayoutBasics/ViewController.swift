b6ab269a61e72d9460ac6feb63c798c5e5566aa8
Rick-Server <rick@rick-server.local>
2014-08-24 14:48:58 +0800

    first commit

---
+++ AutoLayoutBasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutBasics
+//
+//  Created by Rick-Server on 8/24/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
